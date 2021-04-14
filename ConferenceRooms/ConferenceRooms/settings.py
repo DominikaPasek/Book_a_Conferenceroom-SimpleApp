1f60c60ff872e4937d56c6bb7c3510b01b56c5a2
DominikaPasek <76444887+DominikaPasek@users.noreply.github.com>
2021-04-14 07:49:18 +0200

    Update settings.py

--- ConferenceRooms/ConferenceRooms/settings.py
+++ ConferenceRooms/ConferenceRooms/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'conferencerooms_db',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': '********',
         'SERVER': '127.0.0.1'
     }
 }
